6693424ace001fdd1090de62754c50da114d5b99
nive024 <nive0204@gmail.com>
2024-02-01 15:55:09 -0500

    pulled Emily's changes

--- src/ElevatorSystem.java
+++ src/ElevatorSystem.java
@@ -4,7 +4,6 @@ import java.time.LocalTime;
  * A class to test the ElevatorSystem.
  */
 public class ElevatorSystem {
-
     public static void main(String[] args) {
         // TODO: delete this chunk of code before pushing to master
         // testing how LocalTime can be created using a CharSequence
@@ -12,10 +11,11 @@ public class ElevatorSystem {
         System.out.println(time);
         // testing accessor methods for HardwareDevice
         HardwareDevice device = new HardwareDevice(time, 2, FloorButton.UP, 4);
-        System.out.println(device.time());
-        System.out.println(device.floor());
-        System.out.println(device.floorButton());
-        System.out.println(device.carButton());
+        System.out.println(device.getTime());
+        System.out.println(device.getFloor());
+        System.out.println(device.getFloorButton());
+        System.out.println(device.getCarButton());
+        System.out.println(device.getArrived());
 
         // TODO: create a test case showing that your program can read the input file and pass the data back and forth
         // Scheduler is only being used as a communication channel from the Floor to the Elevator and back again
--- src/HardwareDevice.java
+++ src/HardwareDevice.java
@@ -1,17 +1,103 @@
 import java.time.LocalTime;
 
 /**
- * A record to represent the necessary information to pass to the Scheduler.
- *
- * The following accessor methods can be used:
- *      time() - Returns a LocalTime representing when a passenger requests an elevator.
- *      floor() - Returns an integer representing the floor number a passenger requested an elevator at.
- *      floorButton() - Returns a FloorButton representing whether the passenger would like to move up or down.
- *      carButton() - Returns an integer representing the floor number a passenger would like to move to.
- *
- * @param time A LocalTime representing when a passenger requests an elevator.
- * @param floor An integer representing the floor number a passenger requested an elevator at.
- * @param floorButton A FloorButton representing whether the passenger would like to move up or down.
- * @param carButton An integer representing the floor number a passenger would like to move to.
+ * A class to represent the necessary information to pass to the Scheduler.
  */
-public record HardwareDevice(LocalTime time, int floor, FloorButton floorButton, int carButton) { }
+public class HardwareDevice {
+
+    /**
+     * A LocalTime representing when a passenger requests an elevator.
+     */
+    private final LocalTime time;
+
+    /**
+     * An integer representing the floor number a passenger requested an elevator at.
+     */
+    private final int floor;
+
+    /**
+     * A FloorButton representing whether the passenger would like to move up or down.
+     */
+    private final FloorButton floorButton;
+
+    /**
+     * An integer representing the floor number a passenger would like to move to.
+     */
+    private final int carButton;
+
+    /**
+     * True if the elevator has arrived at the floor number a passenger would like to move to. False if not.
+     */
+    private boolean arrived = false;
+
+    /**
+     * Initializes a HardwareDevice with a LocalTime representing when a passenger requests an elevator, an integer
+     * representing the floor number a passenger requested an elevator at, a FloorButton representing whether the
+     * passenger would like to move up or down, and an integer representing the floor number a passenger would like to
+     * move to.
+     *
+     * @param time A LocalTime representing when a passenger requests an elevator.
+     * @param floor An integer representing the floor number a passenger requested an elevator at.
+     * @param floorButton A FloorButton representing whether the passenger would like to move up or down.
+     * @param carButton An integer representing the floor number a passenger would like to move to.
+     */
+    public HardwareDevice (LocalTime time, int floor, FloorButton floorButton, int carButton) {
+        this.time = time;
+        this.floor = floor;
+        this.floorButton = floorButton;
+        this.carButton = carButton;
+    }
+
+    /**
+     * Returns a LocalTime representing when a passenger requests an elevator.
+     *
+     * @return A LocalTime representing when a passenger requests an elevator.
+     */
+    public LocalTime getTime() {
+        return time;
+    }
+
+    /**
+     * Returns an integer representing the floor number a passenger requested an elevator at.
+     *
+     * @return An integer representing the floor number a passenger requested an elevator at.
+     */
+    public int getFloor() {
+        return floor;
+    }
+
+    /**
+     * Returns a FloorButton representing whether the passenger would like to move up or down.
+     *
+     * @return A FloorButton representing whether the passenger would like to move up or down.
+     */
+    public FloorButton getFloorButton() {
+        return floorButton;
+    }
+
+    /**
+     * Returns an integer representing the floor number a passenger would like to move to.
+     *
+     * @return An integer representing the floor number a passenger would like to move to.
+     */
+    public int getCarButton() {
+        return carButton;
+    }
+
+    /**
+     * Returns True if the elevator has arrived at the floor number a passenger would like to move to. False if not.
+     *
+     * @return True if the elevator has arrived at the floor number a passenger would like to move to. False if not.
+     */
+    public boolean getArrived() {
+        return arrived;
+    }
+
+    /**
+     * Sets the Elevator to have arrived at the floor number a passenger would like to move to.
+     */
+    public void setArrived() {
+        this.arrived = true;
+    }
+
+}
