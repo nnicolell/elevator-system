a98939f21db0db0154f32536e6a45cb4f975a6c5
nicolelim <nicolelim@cmail.carleton.ca>
2024-02-23 18:46:38 -0500

    Refactored the state classes to one file

--- src/Elevator.java
+++ src/Elevator.java
@@ -28,7 +28,7 @@ public class Elevator implements Runnable {
         addState("ReachedDestination", new ReachedDestinationState());
         addState("DoorClosing", new DoorClosingState());
         addState("DoorOpening", new DoorOpeningState());
-        addState("NotifyScheduler", new ReachedDestinationState());
+        addState("NotifyScheduler", new NotifySchedulerState());
 
         setState("WaitingForElevatorRequest");
     }
