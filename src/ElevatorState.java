49c238ab39dc79e2abab8982382c81c7ab91afd7
rimshaatif <rimshaatif@cmail.carleton.ca>
2024-02-24 13:17:56 -0500

    Javadocs

--- src/ElevatorState.java
+++ src/ElevatorState.java
@@ -1,5 +1,13 @@
+/**
+ * Interface representing elevator state transition
+ */
 public interface ElevatorState {
 
+        /**
+         * handle events for elevator
+         * @param context stores the current state of the elevator
+         * @param request the hardware device representing the event
+         */
         void handleRequest(Elevator context, HardwareDevice request);
 
         void displayState();
