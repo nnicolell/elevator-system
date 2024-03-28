0edd9ec33901662713b4324558703b1e844d3803
Emily Tang <emilytang2002@gmail.com>
2024-03-28 13:12:59 -0400

    added javadocs to Fault.java

--- src/Fault.java
+++ src/Fault.java
@@ -1,3 +1,6 @@
+/**
+ * An enumerator to represent elevator faults.
+ */
 public enum Fault {
 
     NO_FAULT("No fault"),
@@ -6,19 +9,38 @@ public enum Fault {
     DOORS_NOT_OPENING("Doors not opening"),
     DOORS_NOT_CLOSING("Doors not closing");
 
+    /**
+     * A String representing the fault.
+     */
     private final String faultType;
 
+    /**
+     * Initializes a Fault.
+     *
+     * @param faultType A String representing the fault.
+     */
     Fault(String faultType) {
         this.faultType = faultType;
     }
 
+    /**
+     * Returns a String representing the fault.
+     *
+     * @return A String representing the fault.
+     */
     @Override
     public String toString() {
         return faultType;
     }
 
-    public static Fault stringToFault(String s) {
-        return switch (s) {
+    /**
+     * Returns a Fault representing the specified string.
+     *
+     * @param faultType A String representing the fault.
+     * @return A Fault representing the specified string.
+     */
+    public static Fault stringToFault(String faultType) {
+        return switch (faultType) {
             case "Elevator stuck between floors" -> ELEVATOR_STUCK;
             case "Arrival sensor failed" -> ARRIVAL_SENSOR_FAILED;
             case "Doors not opening" -> DOORS_NOT_OPENING;
@@ -26,4 +48,5 @@ public enum Fault {
             default -> NO_FAULT;
         };
     }
+
 }
