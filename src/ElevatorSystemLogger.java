4002b02bd77c6ab2e3a55bd95ef8976329d3dc77
Emily Tang <emilytang2002@gmail.com>
2024-04-04 20:55:57 -0400

    added comments to ElevatorSystemLogger

--- src/ElevatorSystemLogger.java
+++ src/ElevatorSystemLogger.java
@@ -13,14 +13,18 @@ public class ElevatorSystemLogger extends Logger {
     public ElevatorSystemLogger(String name) {
         super(name, null);
         try {
+            // create a FileHandler for logging to a file
             FileHandler fileHandler = new FileHandler(name + ".log");
             addHandler(fileHandler);
 
             // remove the default console handler to prevent duplicate log messages
             setUseParentHandlers(false);
 
+            // create a ConsoleHandler for logging to the console
             ConsoleHandler consoleHandler = new ConsoleHandler();
             addHandler(consoleHandler);
+
+            // create a custom formatter to include second timing precision in the log messages
             SimpleFormatter formatter = new SimpleFormatter() {
                 @Override
                 public synchronized String format(LogRecord record) {
