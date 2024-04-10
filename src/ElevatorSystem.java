de6eea5dc20da1edef9b8f43d0a9ffd71155bcc7
Emily Tang <emilytang2002@gmail.com>
2024-04-10 01:30:14 -0400

    merged iteration5_Emily into master

--- src/ElevatorSystem.java
+++ src/ElevatorSystem.java
@@ -12,7 +12,7 @@ public class ElevatorSystem {
         elevatorPortNumbers.add(67);
         elevatorPortNumbers.add(69);
         elevatorPortNumbers.add(96);
-        Scheduler scheduler = new Scheduler(elevatorPortNumbers, 5000);
+        Scheduler scheduler = new Scheduler(elevatorPortNumbers);
         Thread schedulerThread = new Thread(scheduler, "Scheduler");
         new ElevatorSystemUI(5, 22, scheduler.getAllElevators());
         schedulerThread.start();
